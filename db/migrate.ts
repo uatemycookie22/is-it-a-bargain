c25b8d3cf4486ad963ea48cfa937cb2141d30b61
Lysander Hernandez <lnandez@amazon.com>
2025-12-25 20:53:39 -0600

    Clean up: Remove all server-side code (BetterAuth, Drizzle, API routes)
    Mobile app now only contains client code. API moved to is-it-a-bargain-api repo.

--- db/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import { db } from "./client";
-
-export function runMigrations() {
-  console.log("Running migrations...");
-  migrate(db, { migrationsFolder: "./drizzle" });
-  console.log("Migrations complete.");
-}
